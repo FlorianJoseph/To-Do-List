1279904b7b857e69ddb50e717a9a8cec45734336
Your Name <flocamj@gmail.com>
2022-10-19 16:30:07 +0200

    Finir les tâches

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,16 +1,47 @@
-const btnNewTask = document.querySelector(".btnNewTask");
-const createTask = document.querySelector("create-task");
+const btnNewTask = document.querySelector(".btn");
+const taskContent = document.querySelector("#task-content");
 const list = document.querySelector("#list");
 
 btnNewTask.addEventListener('click', function(){
-  let createCheckbox = document.createElement('input');
-  
-  createCheckbox.id = `checkbox${document.querySelector("#list p").length}`;
-  createCheckbox.type = "checkbox";
-  list.appendChild(createCheckbox);
+  let createLi = document.createElement('li');
+  list.appendChild(createLi)
 
   let createLabel = document.createElement('label');
-  createLabel.setAttribute("for",`checkbox${document.querySelector("#list label").lenght}`);
-  // createLabel.textContent = 
-  list.appendChild(createLabel);
+  createLabel.id = `label${document.querySelectorAll("#list li").length}`;
+  createLi.appendChild(createLabel);
+
+  let createCheckbox = document.createElement('input');
+  createCheckbox.type = "checkbox";
+  createLabel.appendChild(createCheckbox);
+
+  let createSpan1 = document.createElement('span');
+  createSpan1.className = `check`;
+  createLabel.appendChild(createSpan1);
+  
+  let createSpan2 = document.createElement('span');
+  createLabel.appendChild(createSpan2);
+  createSpan2.style = "margin-left : 2vh";
+  createSpan2.textContent = taskContent.value;
+
+  let createButton = document.createElement('button');
+  createButton.className = `btn`;
+  createLi.appendChild(createButton)
+  createButton.textContent = `Supprimer cette tâche`;
+
+  createButton.addEventListener("click", ()=>{
+    createButton.parentElement.remove()
+  })
+  createSpan2.addEventListener("onclick", () =>{
+    createSpan2.switchColor()
+  })
+  taskContent.value=""
 })
+
+function switchColor(){
+  let cb = document.querySelector(`label>span:last-child`)
+  if (cb.classList.contains('cb-lbl')){
+    cb.classList.remove('cb-lbl')
+  } else {
+    cb.classList.add('cb-lbl')
+  }
+}
